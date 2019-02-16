0f5da04f290b54846b0fd8544034c179ac8e86bc
zhj <1018783112@qq.com>
2019-02-16 10:33:04 +0800

    压缩并合并

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,22 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2019-02-16 09:07:20 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-02-16 10:14:29
+ * @Last Modified time: 2019-02-16 10:29:37
  */
 var gulp = require('gulp');
 var scss = require("gulp-sass");
 var mincss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var server = require("gulp-webserver");
-
+var concat = require("gulp-concat");
 //编译sass
 gulp.task('comscss', function() {
-        return gulp.src("./src/scss/**/*.scss")
-            .pipe(scss())
-            .pipe(gulp.dest('./src/css'))
-    })
-    //压缩css
-gulp.task('zipsass', function() {
-    return gulp.src("./src/css/**/*.css")
-        .pipe(mincss())
-        .pipe(gulp.dest('./build/css'))
+    return gulp.src("./src/scss/**/*.scss")
+        .pipe(scss())
+        .pipe(gulp.dest('./src/css'))
 })
 
-//压缩js
-gulp.task('zipjs', function() {
-        return gulp.src("./src/script/**/*.js")
-            .pipe(uglify())
-            .pipe(gulp.dest('./build/script'))
-    })
-    //启用监听
-gulp.task("auto", function() {
-        return gulp.watch("./src", gulp.series('comscss', "zipsass", "zipjs"))
-    })
-    //开启服务
+//开启服务
 gulp.task("opserver", function() {
     return gulp.src('src')
         .pipe(server({
@@ -42,3 +26,31 @@ gulp.task("opserver", function() {
             livereload: true
         }))
 })
+
+//压缩css
+gulp.task('zipsass', function() {
+    return gulp.src("./src/css/**/*.css")
+        .pipe(mincss())
+        .pipe(gulp.dest('./build/css'))
+})
+
+//合并js
+gulp.task('scripts', function() {
+    return gulp.src('./src/script/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./src/js'));
+});
+
+//压缩js
+gulp.task('zipjs', function() {
+    return gulp.src("./src/script/**/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/script'))
+})
+
+//启用监听
+gulp.task("auto", function() {
+    return gulp.watch("./src", gulp.series('comscss', "zipsass", "zipjs"))
+})
+
+gulp.task('build', gulp.parallel('zipsass', "zipjs"))
